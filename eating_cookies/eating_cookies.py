ceff07ec60142002fd49b9a9c74d645459977242
pdxdave <webdevdave@hotmail.com>
2019-09-11 13:30:47 -0700

    cookies problem solved

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,7 +6,16 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+   # the number of ways to eat negative cookies
+    if n < 0:
+        return 0
+    # the number of ways to eat 0 cookies. 
+    # not eating them is a way I suspect. like deciding not to decide?
+    elif n == 0:
+        return 1
+  
+    count = ( eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache) )
+    return count
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
